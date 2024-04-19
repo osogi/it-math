d1296199d414e43d85ae3c0b6461fed9069918f9
Osogi <dragonmail135159@gmail.com>
2024-04-19 21:46:58 +0300

    task-3: add copressed file struct description + mvp

--- 3/README.md
+++ 3/README.md
@@ -1,1 +1,38 @@
 # Task-3: Сингулярное разложение и сжатие изображений
+
+## Структура сжатого файла
+
+| Name   | Offset | Type   | Description   |
+| ------ | ------ | ------ | ------------- |
+| header | 0x0    | header | File metadata |
+| body   | 0x10   | body   | File payload  |
+
+### Header
+First 16 bytes of the file are allocated for the header.
+
+| Name | Offset | Type    | Description                       |
+| ---- | ------ | ------- | --------------------------------- |
+| n    | 0x0    | uint32  | Horizontal size of original image |
+| m    | 0x4    | uint32  | Vertical size of original image   |
+| k    | 0x8    | uint32  | One of sizes of stored matrices   |
+| mode | 0xC    | char[4] | Order of saved channels           |
+
+### Body
+After file header comes main body, consisting of several consecutive channels (in accordance with channels in the original image).
+
+| Name     | Type   | Description                           |
+| -------- | ------ | ------------------------------------- |
+| channels | chan[] | Compressed channels of original image |
+
+#### Channel
+Each channel is described by `n*k+k+k*m` 32 bit floating point numbers:
+- first `n*k` floats define the matrix U
+- next `k` define the diagonal of matrix S
+- next `k*m` define the matrix Vh
+Where matrices U, S and Vh correspond to matrices $`U`$, $`S`$, and $`V^*`$, from [here](https://en.wikipedia.org/wiki/Singular_value_decomposition#Compact_SVD)
+
+| Name | Type         | Description              |
+| ---- | ------------ | ------------------------ |
+| U    | float32[n*k] | First matrix             |
+| S    | float32[k]   | Second matrix (diagonal) |
+| Vh   | float32[k*m] | Third matrix             |
--- 3/src/main.py
+++ 3/src/main.py
@@ -1,3 +1,4 @@
+from __future__ import annotations
 import os
 from typing import Any, Dict, NamedTuple, Protocol
 import PIL.Image
@@ -9,10 +10,23 @@ import numpy.typing
 
 
 class SVDdata:
-    def __init__(self, a: NamedTuple):
-        self.U: numpy.matrix = a.U
-        self.S: numpy._ArrayFloat_co = a.S
-        self.Vh: numpy.matrix = a.Vh
+    def __init__(self) -> None:
+        self.U: numpy.matrix
+        self.S: numpy._ArrayFloat_co
+        self.Vh: numpy.matrix
+
+    @staticmethod
+    def new(a: NamedTuple) -> SVDdata:
+        self = SVDdata()
+        self.U = a.U
+        self.S = a.S
+        self.Vh = a.Vh
+
+        self.U = numpy.matrix(self.U.astype(numpy.float32))
+        self.S = self.S.astype(numpy.float32)
+        self.Vh = numpy.matrix(self.Vh.astype(numpy.float32))
+
+        return self
 
 
 class SVDer(Protocol):
@@ -21,14 +35,154 @@ class SVDer(Protocol):
 
 class SVDStadartLibrary:
     def svd(self, a: numpy.matrix) -> SVDdata:
-        return SVDdata(numpy.linalg.svd(a))
+        return SVDdata.new(numpy.linalg.svd(a))
+
+
+COMRESS_STRUCT_HEADER_SIZE = 0x10
+FLOAT_SIZE = 4
 
 
 class CompressedData:
-    def __init__(self, a: list[SVDdata]):
+
+    def __init__(self) -> None:
+        self.lst: list[SVDdata] = []
+        self.n: int = -1
+        self.m: int = -1
+        self.k: int = -1
+        self.mode: str = "NONE"
+
+    @staticmethod
+    def new(a: list[SVDdata], mode: str) -> CompressedData:
+        self: CompressedData = CompressedData()
         self.lst = a
         self.n = a[0].U.shape[0]
         self.m = a[0].Vh.shape[1]
+        self.k = len(a[0].S)
+        self.mode = mode
+
+        return self
+
+    def serialize(self):
+        data: bytearray = bytearray()
+
+        # header
+        data.extend(numpy.uint32(self.n).tobytes())
+        data.extend(numpy.uint32(self.m).tobytes())
+        data.extend(numpy.uint32(self.k).tobytes())
+        mode = self.mode[: min(len(self.mode), 4)]
+        data.extend(mode.encode("ASCII"))
+        while len(data) < COMRESS_STRUCT_HEADER_SIZE:
+            data.append(0x0)
+
+        # body
+        for svd in self.lst:
+            data.extend(svd.U.tobytes())
+            data.extend(svd.S.tobytes())
+            data.extend(svd.Vh.tobytes())
+        return data
+
+    def save(self, filename: str):
+        with open(filename, "wb") as f:
+            f.write(self.serialize())
+
+    @staticmethod
+    def deserialize(bts: bytes) -> CompressedData:
+        self: CompressedData = CompressedData()
+
+        # header
+        self.n = int(numpy.frombuffer(bts, offset=0x0, dtype=numpy.uint32, count=1)[0])
+        self.m = int(numpy.frombuffer(bts, offset=0x4, dtype=numpy.uint32, count=1)[0])
+        self.k = int(numpy.frombuffer(bts, offset=0x8, dtype=numpy.uint32, count=1)[0])
+        self.mode = bts[0xC:0x10].decode("ASCII").replace("\x00", "")
+
+        # body
+        body_size = len(bts) - COMRESS_STRUCT_HEADER_SIZE
+        one_channel_size = (self.m + self.n + 1) * self.k * FLOAT_SIZE
+        channel_count = body_size // one_channel_size
+        if one_channel_size * channel_count != body_size:
+            print("Can't deserialize this bytes to CompressedData, some problems with sizes")
+            exit(1)
+        else:
+            self.lst = []
+            usize = self.n * self.k
+            vhsize = self.k * self.m
+            cur_offset = COMRESS_STRUCT_HEADER_SIZE
+            for i in range(channel_count):
+                svd_data = SVDdata()
+                svd_data.U = numpy.matrix(
+                    numpy.frombuffer(bts, offset=cur_offset, dtype=numpy.float32, count=usize).reshape((self.n, self.k))
+                )
+                cur_offset += svd_data.U.size * FLOAT_SIZE
+
+                svd_data.S = numpy.frombuffer(bts, offset=cur_offset, dtype=numpy.float32, count=self.k)
+                cur_offset += svd_data.S.size * FLOAT_SIZE
+
+                svd_data.Vh = numpy.matrix(
+                    numpy.frombuffer(bts, offset=cur_offset, dtype=numpy.float32, count=vhsize).reshape(
+                        (self.k, self.m)
+                    )
+                )
+                cur_offset += svd_data.Vh.size * FLOAT_SIZE
+                self.lst.append(svd_data)
+        return self
+
+    @staticmethod
+    def load(filename: str) -> CompressedData:
+        with open(filename, "rb") as f:
+            return CompressedData.deserialize(f.read())
+
+
+def compress(
+    img: PIL.Image.Image,
+    dst_size: int,
+    svder: SVDer,
+) -> CompressedData:
+    n, m = img.size
+
+    if img.palette is not None:
+        print("Compression for image with palette is not supported")
+        exit(1)
+
+    matrixs: list[numpy.matrix] = [
+        numpy.matrix(numpy.zeros([n, m], dtype=int)) for _ in range(len(img.getpixel((0, 0))))
+    ]
+
+    k = int(min(n, m, (dst_size - COMRESS_STRUCT_HEADER_SIZE) // (len(matrixs) * (m + n + 1) * FLOAT_SIZE)))
+    if k <= 0:
+        print(f"Can't compress image to {dst_size} bytes")
+        exit(1)
+
+    for i in range(n):
+        for j in range(m):
+            clrs = img.getpixel((i, j))
+            for c in range(len(clrs)):
+                matrixs[c][i, j] = clrs[c]
+    svddatas: list[SVDdata] = []
+
+    for m in matrixs:
+        s = svder.svd(m)  # U S Vh
+        s.U = numpy.matrix(s.U.compress([True] * k, axis=1))
+        s.S = s.S[:k]
+        s.Vh = numpy.matrix(s.Vh.compress([True] * k, axis=0))
+        svddatas.append(s)
+
+    return CompressedData.new(svddatas, img.mode)
+
+
+def decompress(cd: CompressedData) -> PIL.Image.Image:
+    img = PIL.Image.new(cd.mode, (cd.n, cd.m))
+
+    matrixes: list[numpy.matrix] = []
+    for s in cd.lst:
+        S = numpy.diag(s.S)
+        a = s.U * S * s.Vh
+        matrixes.append(a)
+
+    for i in range(cd.n):
+        for j in range(cd.m):
+            img.putpixel((i, j), tuple([int(m[i, j]) for m in matrixes]))
+
+    return img
 
 
 MODE_COMPRESS: str = "compress"
@@ -54,63 +208,19 @@ def parse_args() -> Dict[str, Any]:
     return parser.parse_args().__dict__
 
 
-def compress(
-    img: PIL.Image.Image,
-    dst_size: int,
-    svder: SVDer,
-) -> CompressedData:
-    n, m = img.size
-    k = int(min(n, m) * 0.01)
+def filename_and_sz(filename: str):
+    return f"{filename} ({os.path.getsize(filename)} bytes)"
 
-    if img.palette is not None:
-        print("Comprasion for image with palette is not supported")
-        exit(1)
-
-    matrixs: list[numpy.matrix] = [
-        numpy.matrix(numpy.zeros([n, m], dtype=int)) for _ in range(len(img.getpixel((0, 0))))
-    ]
-    for i in range(n):
-        for j in range(m):
-            clrs = img.getpixel((i, j))
-            for c in range(len(clrs)):
-                matrixs[c][i, j] = clrs[c]
-    svddatas: list[SVDdata] = []
-
-    for m in matrixs:
-        s = svder.svd(m)  # U S Vh
-        s.U = numpy.matrix(s.U.compress([True] * k, axis=1))
-        s.S = s.S[:k]
-        s.Vh = numpy.matrix(s.Vh.compress([True] * k, axis=0))
-        svddatas.append(s)
-        # print(s.U)
-
-    return CompressedData(svddatas)
-
-
-def decompress(cd: CompressedData) -> PIL.Image.Image:
-    print(f"{cd.n} {cd.m}")
-    img = PIL.Image.new("RGB", (cd.n, cd.m))
-    
-    matrixes: list[numpy.matrix] = []
-    for s in cd.lst:
-        S = numpy.diag(s.S)
-        a = s.U * S * s.Vh
-        matrixes.append(a)
-    
-    for i in range(cd.n):
-        for j in range(cd.m):
-            img.putpixel((i, j), tuple([int(m[i, j]) for m in matrixes]))
-    
-    return img
 
 def main():
     args = parse_args()
     mode = args["mode"]
+    infile = args["in-file"]
+    outfile = args["out-file"]
     if mode == MODE_COMPRESS:
-        infile = args["in-file"]
         img: PIL.Image.Image = PIL.Image.open(infile)
         bytesize: int = os.path.getsize(infile)
-        finalsize: int = bytesize / args["compression"]
+        finalsize: int = bytesize // args["compression"]
         svder: SVDer
 
         method_str = args["method"]
@@ -125,11 +235,13 @@ def main():
             exit(1)
 
         cd = compress(img, finalsize, svder)
-        img = decompress(cd)
-        img.save(args["out-file"], "BMP")
+        cd.save(outfile)
+        print(f"[Compressed]: {filename_and_sz(infile)} -> {filename_and_sz(outfile)}")
     elif mode == MODE_DECOMPRESS:
-        ...
-        # decompress()
+        cd: CompressedData = CompressedData.load(infile)
+        img = decompress(cd)
+        img.save(outfile, "BMP")
+        print(f"[Decompressed]: {filename_and_sz(infile)} -> {filename_and_sz(outfile)}")
     else:
         print(f"MODE={mode} is not supported")
         exit(1)
